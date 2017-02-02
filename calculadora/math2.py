68f29934085201bd8e35603cbd47af34c4cdcf36
Rafael <comerciorafael@hotmail.com>
2017-02-02 11:01:52 -0200

    Modificado o nome do programa para calculadora.

---
+++ calculadora/math2.py
@@ -0,0 +1,15 @@
+def soma(n1,n2):
+	res = n1+n2
+	return res
+
+def multiplicacao(n1,n2):
+	res = n1*n2
+	return res
+
+def subtracao(n1,n2):
+	res = n1-n2
+	return res
+
+def divisao(n1,n2):
+	res = n1/n2
+	return res
---
+++ calculadora/test.py
@@ -0,0 +1,17 @@
+from math2 import *
+
+def test_soma():
+	assert soma(2,2) == 4
+	assert soma(3,4) == 7
+
+def test_multiplicacao():
+	assert multiplicacao(3,3) == 9
+	assert multiplicacao(2,5) == 10
+
+def test_subtracao():
+	assert subtracao(5,2) == 3
+	assert subtracao(0,4) == -4
+
+def test_divisao():
+	assert divisao(10,2) == 5
+	assert divisao(8,4) == 2
